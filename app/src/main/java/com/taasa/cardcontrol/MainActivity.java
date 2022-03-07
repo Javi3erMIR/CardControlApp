81ca3b53c73402cdad36c19be7c8ebc0f9618d70
Javier Miranda <bernardojaviermirandatarelo@gmail.com>
2022-03-07 12:20:03 -0600

    Main UI finished

---
+++ app/src/main/java/com/taasa/cardcontrol/MainActivity.java
@@ -0,0 +1,25 @@
+package com.taasa.cardcontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private void initComponents(){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initComponents();
+
+    }
+}
