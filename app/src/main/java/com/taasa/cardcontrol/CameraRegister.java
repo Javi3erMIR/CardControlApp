8a3c8a557544ac444d27ef60c03ea8022c0bd85f
Javier Miranda <bernardojaviermirandatarelo@gmail.com>
2022-03-07 12:43:30 -0600

    Camera qr register activity added

---
+++ app/src/main/java/com/taasa/cardcontrol/CameraRegister.java
@@ -0,0 +1,14 @@
+package com.taasa.cardcontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_register);
+    }
+}
